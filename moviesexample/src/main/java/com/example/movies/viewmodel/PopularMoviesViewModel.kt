81ef91de441512568bafc89d3c63a096c2d4404f
Juan Niño <juan.nino@globant.com>
2022-05-25 11:45:40 -0500

    Feature | Implementing coroutines and graphql | Juan Niño

--- moviesexample/src/main/java/com/example/movies/viewmodel/PopularMoviesViewModel.kt
+++ moviesexample/src/main/java/com/example/movies/viewmodel/PopularMoviesViewModel.kt
@@ -1,14 +1,33 @@
 package com.example.movies.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.example.movies.domain.PopularMoviesUseCase
+import androidx.lifecycle.viewModelScope
+import com.example.movies.domain.UseCases
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class PopularMoviesViewModel @Inject constructor(
-    private val getPopularMovies: PopularMoviesUseCase
-): ViewModel(){
+    private val get: UseCases
+): ViewModel() {
 
-    fun getMovies(){
-        getPopularMovies.invoke()
+    fun getBusinessByFoodAndLocation() {
+        viewModelScope.launch {
+            // TODO - replace parameters with real data below
+            get.businessUseCase("pizza", "san francisco")?.let {
+                if (it.search.business?.isNotEmpty() != null) {
+
+                }
+            }
+        }
+    }
+
+    fun getEvents() {
+        viewModelScope.launch {
+            get.eventUseCase()?.let {
+                if(it.eventSearch.events?.isNotEmpty() != null) {
+
+                }
+            }
+        }
     }
 }
